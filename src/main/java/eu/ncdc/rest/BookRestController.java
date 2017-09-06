458b8fd5c72bceeae8a1ce7c5cdaa4c454818081
Justyna Sałacińska <salacinska.justyna@gmail.com>
2017-09-06 12:30:44 +0200

    application done without rest service

--- src/main/java/eu/ncdc/rest/BookRestController.java
+++ src/main/java/eu/ncdc/rest/BookRestController.java
@@ -7,38 +7,88 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.util.List;
 
 import static org.springframework.http.ResponseEntity.created;
 
-@Api(description = "Books resource")
-@RequestMapping(value = UriBuilder.PREFIX + "/books")
 @RestController
 public class BookRestController {
 
-    private BookService bookService;
-    private ModelMapper mapper;
-    private UriBuilder uriBuilder;
-
     @Autowired
-    public BookRestController(BookService bookService, ModelMapper mapper) {
-        this.bookService = bookService;
-        this.mapper = mapper;
-    }
+    private BookService bookService;
 
-    @ApiOperation(value = "Create new book")
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<Void> addBook2(@ApiParam(name = "book")@RequestBody BookDto bookDto){
-        Book book = mapper.map(bookDto, Book.class);
-        bookService.saveBook(book);
-        URI uri = uriBuilder. requestUriWithId(book.getId());
-        return created(uri).build();
-    }
+//    @RequestMapping(value = "/book", method = RequestMethod.POST)
+//    public ResponseEntity<Book> addBook(@RequestBody Book book) {
+//        HttpHeaders headers = new HttpHeaders();
+//        if (book == null) {
+//            return new ResponseEntity<Book>(HttpStatus.BAD_REQUEST);
+//        }
+//        bookService.saveBook(book);
+//        headers.add("Book added  - ", String.valueOf(book.getId()));
+//        return new ResponseEntity<Book>(book, headers, HttpStatus.CREATED);
+//    }
+//
+
+//    @RequestMapping(value = "/list")
+//    public List<Book> getAllBooks(){
+//        return bookService.findAllBooks();
+//    }
+//    @RequestMapping(value = "/add", method = RequestMethod.POST)
+//    public String addBook3(@RequestBody Book book){
+//        bookService.saveBook(book);
+//        return "OK";
+//    }
+
+
+//
+//    @RequestMapping(value = "/add/{author}/{title}/{isbn}")
+//    public String addBook2(@PathVariable Book book){
+//        bookService.saveBook(book);
+//        return "OK";
+//    }
+
+//    @GetMapping("/books")
+//    public List getAllBooks() {
+//        return bookService.findAllBooks();
+//    }
+
+//    @RequestMapping(value = "/book", method = RequestMethod.GET)
+//    public ResponseEntity<List<Book>> getAllBooks() {
+//        List<Book> books = bookService.findAllBooks();
+//
+//        if(books.isEmpty()){
+//            return new ResponseEntity<List<Book>>(HttpStatus.NO_CONTENT);
+//        }
+//        return new ResponseEntity<List<Book>>(books, HttpStatus.OK);
+//    }
+
+//    private BookService bookService;
+//    private ModelMapper mapper;
+//    private UriBuilder uriBuilder;
+//
+//    @Autowired
+//    public BookRestController(BookService bookService, ModelMapper mapper) {
+//        this.bookService = bookService;
+//        this.mapper = mapper;
+//    }
+//
+//    @ApiOperation(value = "Create new book")
+//    @RequestMapping(method = RequestMethod.POST)
+//    public ResponseEntity<Void> addBook2(@ApiParam(name = "book")@RequestBody BookDto bookDto){
+//        Book book = mapper.map(bookDto, Book.class);
+//        bookService.saveBook(book);
+//        URI uri = uriBuilder. requestUriWithId(book.getId());
+//        return created(uri).build();
+//    }
 
 //    @ApiOperation(value = "Get all books", response = BookPageDto.class)
 //    @RequestMapping(method = RequestMethod.GET)
--- src/main/java/eu/ncdc/service/BookService.java
+++ src/main/java/eu/ncdc/service/BookService.java
@@ -1,13 +1,8 @@
 package eu.ncdc.service;
 
 import eu.ncdc.dao.BookDao;
-import eu.ncdc.dao.BookDaoImpl;
 import eu.ncdc.entity.Book;
-import eu.ncdc.rest.ResultPage;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,16 +13,14 @@ public class BookService {
     @Autowired
     private BookDao bookDao;
 
-    public void saveBook(Book book){
+    public Book saveBook(Book book){
         bookDao.saveBook(book);
+        return book;
     }
 
     public Book findBookById(Integer id){ return bookDao.findBookById(id); }
 
     public List<Book> findAllBooks(){return bookDao.findAllBooks();}
 
-//    public ResultPage<Book> getBook(int pageNumber, int pageSize) {
-//        Page<Book> bookPage = bookDao.findAllBooks(new PageRequest(pageNumber, pageSize));
-//        return new ResultPage<>(bookPage.getContent(), bookPage.getNumber(), bookPage.getTotalPages());
-//    }
 }
+
